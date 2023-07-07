c3221f5a8728bf59b11bd2752b83ddd0ffbac874
Umisyus <umit007_@hotmail.com>
2023-07-07 04:00:59 +0000

    Updated input schema and input field

--- src/main.ts
+++ src/main.ts
@@ -7,21 +7,21 @@ import fetch from 'node-fetch'
 
 // Get input of the actor.
 
-let { threshold, profileName } = await Actor.getInput<any>();
+let { threshold, profileName, json_dataset } = await Actor.getInput<any>();
 console.log(profileName, threshold);
 
 log.info(`threshold: ${threshold}, profileName: ${profileName}`)
 if (!profileName) throw new Error('No username specified! Please specify a username to crawl.')
 
-await getData(profileName, threshold)
+await getData(profileName, threshold, json_dataset ?? 'pinterest-json' + new Date().toLocaleString())
 
 // Exit successfully
 await Actor.exit();
 
-async function getData(userName = 'dracana96', THRESHOLD = 100) {
+async function getData(userName:string, THRESHOLD = 100, json_dataset:string) {
     // NODE VERSION
 
-    const ds = await Actor.openKeyValueStore('pinterest-json-TESTING-JSON-KVS')
+    const ds = await Actor.openKeyValueStore(json_dataset)
 
     console.log(userName)
     let pins_url_bookmark = (userName: string, bookmark: string) => `https://www.pinterest.ca/resource/UserPinsResource/get/?source_url=%2F${userName}%2Fpins%2F&data=%7B%22options%22%3A%7B%22is_own_profile_pins%22%3Atrue%2C%22username%22%3A%22${userName}%22%2C%22field_set_key%22%3A%22grid_item%22%2C%22pin_filter%22%3Anull%2C%22bookmarks%22%3A%5B%22${bookmark}%22%5D%7D%2C%22context%22%3A%7B%7D%7D&_=1670393784068`
