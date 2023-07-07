b3a12545d52c0b6f15d1286904b49aba29cf43a3
Umisyus <umit007_@hotmail.com>
2023-07-07 05:57:20 +0000

    fix dataset name

--- src/main.ts
+++ src/main.ts
@@ -15,22 +15,33 @@ if (!profileName) throw new Error('No username specified! Please specify a usern
 
 const regex = /[\s\,\/\:]/ig;
 
-let d_str = ('pinterest-json-' + new Date().toLocaleString())
-  .replace(regex, '-')
-  .replace(/-{2,}/,'_')
-  .replace(/-$/,'')
-  .replace(/^-/,'')
+let fmt_ds_name = (s:string) =>
+ s
+.replace(regex, '-')
+.replace(/-{2,}/,'_')
+.replace(/-$/,'')
+.replace(/^-/,'')
+.replace(/_/g,'-')
 
-await getData(profileName, threshold, json_dataset ?? d_str)
+let d_str = fmt_ds_name(json_dataset)
 
+try { 
+
+await getData(profileName, threshold, d_str)
+
+} catch(e) {
+console.log(d_str);
+console.error(e);
+
+}
 // Exit successfully
 await Actor.exit();
 
-async function getData(userName:string, THRESHOLD = 100, json_dataset:string) {
+async function getData(userName:string, THRESHOLD = 100, json_dataset_name:string) {
     // NODE VERSION
 console.log(`Saving to: ${json_dataset}`);
 
-    const ds = await Actor.openKeyValueStore(json_dataset)
+    const ds = await Actor.openKeyValueStore(json_dataset_name)
 
     console.log(userName)
     let pins_url_bookmark = (userName: string, bookmark: string) => `https://www.pinterest.ca/resource/UserPinsResource/get/?source_url=%2F${userName}%2Fpins%2F&data=%7B%22options%22%3A%7B%22is_own_profile_pins%22%3Atrue%2C%22username%22%3A%22${userName}%22%2C%22field_set_key%22%3A%22grid_item%22%2C%22pin_filter%22%3Anull%2C%22bookmarks%22%3A%5B%22${bookmark}%22%5D%7D%2C%22context%22%3A%7B%7D%7D&_=1670393784068`
